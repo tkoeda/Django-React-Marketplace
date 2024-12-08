cb659eac9e1157370360498d8b1693ed2774f1a5
Tainosuke Koeda <gwckoeda@gmail.com>
2024-12-08 19:56:18 +0900

    use django-environ instead

--- backend/core/deployment_settings.py
+++ backend/core/deployment_settings.py
@@ -2,12 +2,18 @@ import os
 import dj_database_url
 from .settings import *
 from .settings import BASE_DIR
+import environ
 
-ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+
+ALLOWED_HOSTS = [env('RENDER_EXTERNAL_HOSTNAME')]
+CSRF_TRUSTED_ORIGINS = ['https://' + env('RENDER_EXTERNAL_HOSTNAME')]
 
 DEBUG = False
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -21,9 +27,9 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173'
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:5173'
+# ]
 
 STORAGES = {
     "default": {
@@ -36,7 +42,7 @@ STORAGES = {
 
 DATABASES = {
     'default': dj_database_url.config(
-        default = os.environ.get('DATABASE_URL'),
+        default = env('DATABASE_URL'),
         conn_max_age = 600
     )
 }
@@ -46,11 +52,11 @@ AUTH_USER_MODEL = 'users.User'
 
 # S3 Configuration
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')  # Replace with your actual bucket name
-AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')  # Replace with your actual bucket name
+AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_QUERYSTRING_EXPIRE = 600
-AWS_S3_CUSTOM_DOMAIN = os.environ.get('CLOUDFRONT_DISTRIBUTION_DOMAIN_NAME')
+AWS_S3_CUSTOM_DOMAIN = env('CLOUDFRONT_DISTRIBUTION_DOMAIN_NAME')
