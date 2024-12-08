3c7bc39fffd8ec72388b996aab5aae8433f3b58a
Tainosuke Koeda <gwckoeda@gmail.com>
2024-12-08 18:54:55 +0900

    change deploy settings

--- backend/core/deployment_settings.py
+++ backend/core/deployment_settings.py
@@ -4,7 +4,7 @@ import environ
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [os.environ.get(['RENDER_EXTERNAL_HOSTNAME'])]
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get(['RENDER_EXTERNAL_HOSTNAME'])]
 
 DEBUG = False
