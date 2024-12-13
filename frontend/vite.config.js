4115dfac596a9e6d28673c0db2a52da92d89a245
Tainosuke Koeda <gwckoeda@gmail.com>
2024-12-14 00:43:20 +0900

    fix(chunkSizeWarningLimit): not sure what this is

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  envDir: path.resolve(__dirname, '..'),
-  css: {
-    postcss: {
-      plugins: [],
+    plugins: [react()],
+    envDir: path.resolve(__dirname, ".."),
+    css: {
+        postcss: {
+            plugins: [],
+        },
     },
-  }
-})
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return id
+                            .toString()
+                            .split("node_modules/")[1]
+                            .split("/")[0]
+                            .toString();
+                    }
+                },
+            },
+        },
+    },
+});
