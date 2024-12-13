b5b70ce9b7ed38ef80d09157e315121f4ae50753
Tainosuke Koeda <gwckoeda@gmail.com>
2024-12-13 23:45:10 +0900

    feat(constants): add constants for listing status

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,9 @@
 export const ACCESS_TOKEN = "access_token";
 export const REFRESH_TOKEN = "refresh_token";
 export const USER_ID = "user_id";
+export const LISTING_STATUS = {
+    DRAFT: "draft",
+    PUBLISHED: "published",
+    IN_PROGRESS: "in_progress",
+    COMPLETED: "completed",
+};
