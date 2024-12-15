8457b409e5f1940b079ce883d359d6d1d82bf8a4
Tainosuke Koeda <gwckoeda@gmail.com>
2024-12-15 23:56:43 +0900

    fix(cors): cors allow credentials

--- backend/core/deployment_settings.py
+++ backend/core/deployment_settings.py
@@ -17,6 +17,7 @@ DEBUG = False
 SECRET_KEY = env("SECRET_KEY")
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -25,8 +26,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://marketplace-react-rz2n.onrender.com",
@@ -54,9 +56,7 @@ AUTH_USER_MODEL = "users.User"
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = env(
-    "AWS_STORAGE_BUCKET_NAME"
-)  # Replace with your actual bucket name
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")  
 AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
