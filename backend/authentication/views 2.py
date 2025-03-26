f8df3ae0c8666f3b1abfbc33e917c9192579fcb4
Tainosuke Koeda <67545532+tkoeda@users.noreply.github.com>
2025-03-26 10:59:43 +0900

    Delete backend/authentication/views 2.py

--- backend/authentication/views 2.py
+++
@@ -1,5 +0,0 @@
-from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import MyTokenObtainPairSerializer
-# Create your views here.
-class MyTokenObtainPairView(TokenObtainPairView):
-    serializer_class = MyTokenObtainPairSerializer
